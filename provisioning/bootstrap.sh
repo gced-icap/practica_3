1606e6415ed606ceb7f1154f76fca6a2648cd067
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2023-10-03 12:05:20 +0200

    Configuración de nombres actualizada y ajuste del PATH

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Sintaxis: $0 SERVER_IP CLIENT_IP"
-    exit
+if [ "$#" -ne 4 ]; then
+    echo "Sintaxis: $0 SERVER_IP CLIENT_IP SERVER_HOSTNAME CLIENT_HOSTNAME"
+    exit -1
 fi
 
 SERVER_IP=$1
 CLIENT_IP=$2
+SERVER_HOSTNAME=$3
+CLIENT_HOSTNAME=$4
 
 apt-get update
 # Install basic software
-apt-get install -y vim nano sshpass unzip dnsutils dos2unix whois lvm2 mdadm nfs-kernel-server nfs-common
+apt-get install -y vim nano sshpass unzip dnsutils dos2unix whois fdisk xfsprogs lvm2 mdadm nfs-kernel-server nfs-common
 
 timedatectl set-timezone Europe/Madrid
 
 # Populate /etc/hosts
 sed -i "/server/d" /etc/hosts
 sed -i "/client/d" /etc/hosts    
-echo -e "$SERVER_IP \t server" >> /etc/hosts
-echo -e "$CLIENT_IP \t client" >> /etc/hosts
+echo -e "$SERVER_IP \t $SERVER_HOSTNAME" >> /etc/hosts
+echo -e "$CLIENT_IP \t $CLIENT_HOSTNAME" >> /etc/hosts
+
+# Set $PATH for vagrant
+sed -i "/sbin/d" /home/vagrant/.profile
+echo 'PATH=/sbin:$PATH' >> /home/vagrant/.profile
