81d7eb9aa13ff79b1ff02d17029a6577c0b2017d
Roberto R. Expósito <roberto.rey.exposito@udc.es>
2024-10-11 12:14:00 +0200

    Establécese un contrasinal ao root

--- provisioning/bootstrap.sh
+++ provisioning/bootstrap.sh
@@ -15,12 +15,18 @@ apt-get update
 apt-get install -y vim nano sshpass unzip dnsutils dos2unix whois fdisk xfsprogs lvm2 mdadm nfs-kernel-server nfs-common
 
 timedatectl set-timezone Europe/Madrid
+passwd -d root
+echo 'root:vagrant' | chpasswd -m
 
 # Populate /etc/hosts
 sed -i "/server/d" /etc/hosts
 sed -i "/client/d" /etc/hosts    
 echo -e "$SERVER_IP \t $SERVER_HOSTNAME" >> /etc/hosts
 echo -e "$CLIENT_IP \t $CLIENT_HOSTNAME" >> /etc/hosts
+
+# SSH config
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl restart sshd
 
 # Set $PATH for vagrant
 sed -i "/sbin/d" /home/vagrant/.profile
